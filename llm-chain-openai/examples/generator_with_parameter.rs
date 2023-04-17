fbd1aacfc536c06aea128d39d7531efdf490db06
William Högman Rudenmalm <me@whn.se>
2023-04-17 21:05:38 +0200

    docs: prepare for 0.6.0

--- llm-chain-openai/examples/generator_with_parameter.rs
+++ llm-chain-openai/examples/generator_with_parameter.rs
@@ -1,15 +1,26 @@
-use llm_chain::{prompt, traits::StepExt};
+use llm_chain::{prompt, traits::StepExt, Parameters};
 use llm_chain_openai::chatgpt::{Executor, Step};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a new ChatGPT executor
     let exec = Executor::new_default();
-    let res = Step::for_prompt(prompt!(
+    // Create our step containing our prompt template
+    let step = Step::for_prompt(prompt!(
         "You are a bot for making personalized greetings",
-        "Make a personalized greeting tweet for {text}"
-    ))
-    .run(&("Emil".into()), &exec)
-    .await?;
+        "Make a personalized greeting tweet for {{text}}" // Text is the default parameter name, but you can use whatever you want
+    ));
+
+    // A greeting for emil!
+    let res = step.run(&Parameters::new_with_text("Emil"), &exec).await?;
     println!("{}", res);
+
+    // A greeting for you
+    let res = step
+        .run(&Parameters::new_with_text("Your Name Here"), &exec)
+        .await?;
+
+    println!("{}", res);
+
     Ok(())
 }
--- llm-chain-openai/examples/sequential_generation.rs
+++ llm-chain-openai/examples/sequential_generation.rs
@@ -3,21 +3,32 @@ use llm_chain_openai::chatgpt::{Executor, Step};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    // Create a new ChatGPT executor with the default settings
     let exec = Executor::new_default();
+
+    // Create a chain of steps with two prompts
     let chain = Chain::new(vec![
+        // First step: make a personalized birthday email
         Step::for_prompt(
             prompt!("You are a bot for making personalized greetings", "Make personalized birthday e-mail to the whole company for {{name}} who has their birthday on {{date}}. Include their name")
         ),
+
+        // Second step: summarize the email into a tweet. Importantly, the text parameter becomes the result of the previous prompt.
         Step::for_prompt(
             prompt!( "You are an assistant for managing social media accounts for a company", "Summarize this email into a tweet to be sent by the company, use emoji if you can. \n--\n{{text}}")
         )
     ]);
+
+    // Run the chain with the provided parameters
     let res = chain
         .run(
+            // Create a Parameters object with key-value pairs for the placeholders
             vec![("name", "Emil"), ("date", "February 30th 2023")].into(),
             &exec,
         )
         .await
         .unwrap();
+
+    // Print the result to the console
     println!("{:?}", res);
 }
