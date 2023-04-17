3690150f902fe289a1dc2d464604c2f005d82cd8
William Högman Rudenmalm <me@whn.se>
2023-04-17 19:46:57 +0200

    refactor: deprecate Step::new

--- llm-chain-llama/src/step.rs
+++ llm-chain-llama/src/step.rs
@@ -75,6 +75,7 @@ impl Step {
     ///
     /// * `prompt` - The prompt template for the step.
     /// * `config` - An optional configuration for the step. If `None`, the default configuration will be used.
+    #[deprecated(since = "0.6.0", note = "Use for_rust_prompt instead")]
     pub fn new_with_config<P: Into<PromptTemplate>>(
         prompt: P,
         config: Option<LlamaConfig>,
@@ -90,6 +91,7 @@ impl Step {
     /// # Arguments
     ///
     /// * `prompt` - The prompt template for the step.
+    #[deprecated(since = "0.6.0", note = "Use for_prompt instead")]
     pub fn new<P: Into<PromptTemplate>>(prompt: P) -> Self {
         Self::new_with_config(prompt, None)
     }
@@ -112,6 +114,7 @@ impl Step {
         prompt: P,
         config: Option<LlamaConfig>,
     ) -> Self {
+        #[allow(deprecated)]
         Self::new_with_config(prompt.as_text_prompt_or_convert(), config)
     }
 }
