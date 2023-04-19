782f81ecac9cdd14d1ef9ed408eca9fa90cfa2a4
Paweł Biłko <pbilko@pop-os.localdomain>
2023-04-19 16:08:28 +0200

    Renamed toolbox to multitool

--- llm-chain/src/tools/multitool_default.rs
+++ llm-chain/src/tools/multitool_default.rs
@@ -1,11 +1,11 @@
-use crate::toolbox;
+use crate::multitool;
 use crate::tools::tools::{
     BashTool, BashToolError, ExitTool, ExitToolError, PythonTool, PythonToolError,
 };
 use crate::tools::{Tool, ToolDescription, ToolError};
 use thiserror::Error;
 
-toolbox!(
+multitool!(
     DefaultToolbox,
     DefaultToolboxError,
     BashTool,
