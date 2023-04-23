507586a0c237f58f1575f3a2f7daa8f121369c6c
William Högman Rudenmalm <me@whn.se>
2023-04-23 10:05:42 +0200

    docs: add docs for sequential chain

--- llm-chain/src/chains/sequential.rs
+++ llm-chain/src/chains/sequential.rs
@@ -1,3 +1,30 @@
+//! A module for implementing a sequential chain of LLM steps.
+//!
+//! This module provides the `Chain` struct, which represents a sequential chain of steps for Large Language Models (LLMs). Each step in the chain is executed in order, and the output of the previous step is available as input to the next step.
+//!
+//! The `Chain` struct allows you to:
+//! - Create a new chain with a vector of `Step` instances
+//! - Execute the chain with a given set of `Parameters` and an `Executor`
+//!
+//! The `Chain` struct is designed to work with any executor that implements the `Executor` trait, providing flexibility and extensibility.
+//!
+//! # Example
+//!
+//! ```ignore
+//!
+//! // Assuming an executor `executor` that implements the `Executor` trait.
+//! let step1 = Step::new(prompt!("Write a summary for this text: {{text}}"));
+//! let step2 = Step::new(prompt!("{{text}}\n\nWrite a tweet thread for the above summary");
+//!
+//! let chain = Chain::new(vec![step1, step2]);
+//!
+//! let parameters = parameters!("your input text here")
+//!
+//! // Execute the chain with the provided parameters and executor.
+//! let result = chain.run(parameters, &executor).await;
+//! ```
+//!
+//! This module also provides serialization and deserialization support for the `Chain` struct, allowing you to store and load chains using formats like JSON, YAML, or others.
 use serde::de::{Deserializer, MapAccess};
 use serde::ser::{SerializeMap, Serializer};
 use serde::{Deserialize, Serialize};
@@ -20,20 +47,46 @@ pub enum SequentialChainError<Err: ExecutorError> {
     #[error("The vector of steps was empty")]
     NoSteps,
 }
-// A sequential chain is a chain where each step is executed in order, with the output of the previous being available to the next.
+
+/// A sequential chain is a chain where each step is executed in order, with the output of the previous step being available to the next step.
 #[derive(Clone, Debug)]
 pub struct Chain<E: Executor> {
     steps: Vec<Step<E>>,
 }
 
 impl<E: Executor> Chain<E> {
+    /// Creates a new `Chain` instance with the given sequence of steps.
+    ///
+    /// # Arguments
+    ///
+    /// * `steps` - A vector of `Step<E>` objects that define the sequence of steps for the chain.
     pub fn new(steps: Vec<Step<E>>) -> Chain<E> {
         Chain { steps }
     }
+
+    /// Creates a new `Chain` instance with a single step.
+    ///
+    /// # Arguments
+    ///
+    /// * `step` - A `Step<E>` object that defines the single step for the chain.
     pub fn of_one(step: Step<E>) -> Chain<E> {
         Chain { steps: vec![step] }
     }
 
+    /// Executes the chain with the given parameters and executor.
+    ///
+    /// This method runs each step in the chain in sequence, passing the output of the previous step to the next step.
+    /// If the chain is empty, an error is returned.
+    ///
+    /// # Arguments
+    ///
+    /// * `parameters` - A `Parameters` object containing the input parameters for the chain.
+    /// * `executor` - A reference to an executor that implements the `Executor` trait.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(E::Output)` - If the chain executes successfully, the output of the last step is returned.
+    /// * `Err(SequentialChainError<E::Error>)` - If an error occurs during the execution of the chain, the error is returned.
     pub async fn run(
         &self,
         parameters: Parameters,
