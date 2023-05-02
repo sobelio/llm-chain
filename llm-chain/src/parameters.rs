4b080064af5837e30e18e4efb39fdd17bb11352d
William Högman Rudenmalm <me@whn.se>
2023-05-02 21:32:27 +0200

    add support for dynamic parameters

--- llm-chain/src/parameters.rs
+++ llm-chain/src/parameters.rs
@@ -2,8 +2,12 @@
 //!
 //! Parameters are used to pass data between steps of the chain. They are used to fill in the prompt template, and are also filled in by the output of the previous step. Parameters have a special key, `text`, which is used as a default key for simple use cases.
 use crate::output::Output;
-use dynfmt::{Argument, FormatArgs};
-use std::collections::HashMap;
+use std::{
+    collections::{BTreeMap, HashMap},
+    fmt::Debug,
+};
+
+type Map = BTreeMap<String, Box<dyn ParamFull>>;
 
 /// Parameters define the parameters sent into each step. The parameters are used to fill in the prompt template, and are also filled in by the output of the previous step. Parameters have a special key, `text`, which is used as a default key for simple use cases.
 ///
@@ -24,27 +28,58 @@ use std::collections::HashMap;
 /// assert_eq!(p.get("text").unwrap().as_str(), "Hello world!");
 /// assert_eq!(p.get("name").unwrap().as_str(), "John Doe");
 /// ```
-#[derive(Clone, Default, Debug)]
+///
+///
+#[derive(Default, Debug)]
 pub struct Parameters {
-    map: HashMap<String, String>,
+    map: Map,
+}
+
+impl Clone for Parameters {
+    fn clone(&self) -> Self {
+        let mut map = Map::new();
+        for (key, value) in self.map.iter() {
+            map.insert(key.clone(), value.boxed_clone());
+        }
+        Self { map }
+    }
+}
+
+pub trait Param {
+    fn get(&self) -> String;
+}
+
+/// This trait is used to implement a dynamic parameter this shouldn't be used but exists only for internal purposes.
+#[doc(hidden)]
+pub trait ParamFull: Param + Debug {
+    #[doc(hidden)]
+    fn boxed_clone(&self) -> Box<dyn ParamFull>;
+}
+
+impl<T: Param + Debug + Clone + 'static> ParamFull for T {
+    #[doc(hidden)]
+    fn boxed_clone(&self) -> Box<dyn ParamFull> {
+        Box::new(self.clone())
+    }
+}
+#[derive(Debug, Clone)]
+struct StringParam {
+    value: String,
+}
+
+impl StringParam {
+    fn new(value: String) -> Self {
+        Self { value }
+    }
+}
+
+impl Param for StringParam {
+    fn get(&self) -> String {
+        self.value.clone()
+    }
 }
 
 const TEXT_KEY: &str = "text";
-
-impl FormatArgs for Parameters {
-    fn get_index(&self, index: usize) -> Result<Option<Argument<'_>>, ()> {
-        let res = if index == 0 {
-            self.get_key(TEXT_KEY)?
-        } else {
-            self.map.get_index(index)?
-        };
-        Ok(res)
-    }
-
-    fn get_key(&self, key: &str) -> Result<Option<Argument<'_>>, ()> {
-        self.map.get_key(key)
-    }
-}
 
 impl Parameters {
     /// Creates a new empty set of parameters.
@@ -53,16 +88,28 @@ impl Parameters {
     }
     /// Creates a new set of parameters with a single key, `text`, set to the given value.
     pub fn new_with_text<T: Into<String>>(text: T) -> Parameters {
-        let mut map = HashMap::new();
-        map.insert(TEXT_KEY.to_string(), text.into());
+        let mut map = Map::new();
+        map.insert(
+            TEXT_KEY.to_string(),
+            Box::new(StringParam::new(text.into())),
+        );
         Parameters { map }
     }
     /// Copies the parameters and adds a new key-value pair.
     pub fn with<K: Into<String>, V: Into<String>>(&self, key: K, value: V) -> Parameters {
         let mut copy = self.clone();
-        copy.map.insert(key.into(), value.into());
+        copy.map
+            .insert(key.into(), Box::new(StringParam::new(value.into())));
         copy
     }
+
+    /// Copies the parameters and adds a new key-value pair pair, where the value is a dynamic parameter.
+    pub fn with_dynamic<K: Into<String>, V: ParamFull>(&self, key: K, value: V) -> Parameters {
+        let mut copy = self.clone();
+        copy.map.insert(key.into(), value.boxed_clone());
+        copy
+    }
+
     /// Copies the parameters and adds a new key-value pair with the key `text`, which is the default key.
     pub fn with_text<K: Into<String>>(&self, text: K) -> Parameters {
         self.with(TEXT_KEY, text)
@@ -76,54 +123,39 @@ impl Parameters {
     /// Combines two sets of parameters, returning a new set of parameters with all the keys from both sets.
     pub fn combine(&self, other: &Parameters) -> Parameters {
         let mut copy = self.clone();
-        copy.map.extend(other.map.clone());
+        for (key, value) in other.map.iter() {
+            copy.map.insert(key.clone(), value.boxed_clone());
+        }
         copy
     }
     /// Returns the value of the given key, or `None` if the key does not exist.
-    pub fn get(&self, key: &str) -> Option<&String> {
-        self.map.get(key)
+    pub fn get(&self, key: &str) -> Option<String> {
+        self.map.get(key).map(|param| param.get())
     }
 
-    pub fn forked<K, V1, V2>(&self, key: K, a: V1, b: V2) -> (Parameters, Parameters)
-    where
-        K: Into<String> + Copy,
-        V1: Into<String>,
-        V2: Into<String>,
-    {
-        let mut copy = self.clone();
-        copy.map.insert(key.into(), a.into());
-        let mut copy2 = self.clone();
-        copy2.map.insert(key.into(), b.into());
-        (copy, copy2)
-    }
-
-    pub fn forked_text<V1, V2>(&self, a: V1, b: V2) -> (Parameters, Parameters)
-    where
-        V1: Into<String>,
-        V2: Into<String>,
-    {
-        self.forked(TEXT_KEY, a, b)
-    }
-
-    pub fn get_text(&self) -> Option<&String> {
+    pub fn get_text(&self) -> Option<String> {
         self.get(TEXT_KEY)
-    }
-
-    /// Returns new `Parameters` with all the values replaced with placeholders.
-    pub fn with_placeholder_values(&self) -> Parameters {
-        let mut copy = self.clone();
-        for (key, value) in copy.map.iter_mut() {
-            *value = format!("{{{}}}", key);
-        }
-        copy
     }
 
     pub(crate) fn to_tera(&self) -> tera::Context {
         let mut context = tera::Context::new();
         for (key, value) in self.map.iter() {
-            context.insert(key, value);
+            context.insert(key, &value.get());
         }
         context
+    }
+
+    fn from_seq<K, V, M>(m: M) -> Self
+    where
+        K: Into<String>,
+        V: Into<String>,
+        M: IntoIterator<Item = (K, V)>,
+    {
+        let mut map = Map::new();
+        for (k, v) in m.into_iter() {
+            map.insert(k.into(), Box::new(StringParam::new(v.into())));
+        }
+        Parameters { map }
     }
 }
 
@@ -141,24 +173,25 @@ impl From<&str> for Parameters {
 
 impl From<HashMap<String, String>> for Parameters {
     fn from(map: HashMap<String, String>) -> Self {
-        Parameters { map }
+        Parameters::from_seq(map.into_iter())
+    }
+}
+
+impl From<BTreeMap<String, String>> for Parameters {
+    fn from(map: BTreeMap<String, String>) -> Self {
+        Parameters::from_seq(map.into_iter())
     }
 }
 
 impl From<Vec<(String, String)>> for Parameters {
     fn from(data: Vec<(String, String)>) -> Self {
-        let map: HashMap<String, String> = data.into_iter().collect();
-        Parameters { map }
+        Parameters::from_seq(data.into_iter())
     }
 }
 
 impl From<Vec<(&str, &str)>> for Parameters {
     fn from(data: Vec<(&str, &str)>) -> Self {
-        let map: HashMap<String, String> = data
-            .into_iter()
-            .map(|(k, v)| (k.to_string(), v.to_string()))
-            .collect();
-        Parameters { map }
+        Parameters::from_seq(data)
     }
 }
 
