8db32953dde6871bb9c4a4abcff0c75046bd3a97
Emil Juzovitski <ejuzov@gmail.com>
2023-05-02 23:47:54 +0200

    clarity: few_shot restructuring

--- llm-chain-llama/examples/few_shot.rs
+++ llm-chain-llama/examples/few_shot.rs
@@ -18,17 +18,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut context_params = ContextParams::new();
     context_params.n_ctx = 2048; // default is 512
     let exc_options = PerExecutor::new().with_context_params(context_params);
-    let exec = executor!(llama, exc_options, inv_options.clone())?;
+    let exec_1 = executor!(llama, exc_options.clone(), inv_options.clone())?;
 
-    let system_prompt = "Given two words, you are tasked to concatenate the last letter of each word together. answer as Assistant:";
-    let user_prompt = "Take the last letters of the words in {{ full_name }} and concatenate them";
+    let user_prompt = "Take the last letters of the words in '{{ full_name }}' and concatenate them";
     let res =
-        Step::for_prompt_template(prompt!(system_prompt, user_prompt))
-            .run(&parameters!().with("full_name", "Elon Musk"), &exec)
+        Step::for_prompt_template(prompt!(user: user_prompt))
+            .run(&parameters!().with("full_name", "Elon Musk"), &exec_1)
             .await?;
     println!("{} (zero-shot answer)", res); // probably not correct
     let conversation = Conversation::new()
-        .with_system(system_prompt.to_string())
         .with_user_template(
             user_prompt,
             &parameters!().with("full_name", "Saquon Barkley"),
@@ -43,14 +41,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             user_prompt,
             &parameters!().with("full_name", "Julius Ceasar"),
         )?
-        .with_assistant("JC".to_string());
+        .with_assistant("JC".to_string())
+        .with_user_template(
+            user_prompt,
+            &parameters!().with("full_name", "Ding Liren"),
+        )?
+        .with_assistant("DL".to_string());
     // build a chain, that has the above conversation stored in its state
     let mut chain = Chain::new_with_message_collection(&conversation);
     // Try the Elon Musk problem again
     // Define the step
-    let step = Step::for_prompt_and_options(prompt!(user: user_prompt), inv_options);
+    let step = Step::for_prompt_template(prompt!(user: user_prompt));
     // Execute the chain.
-    let res = chain.send_message(step, &parameters!().with("full_name", "Elon Musk"), &exec).await?;
+    let exec_2 = executor!(llama, exc_options, inv_options)?;
+    let res = chain.send_message(step, &parameters!().with("full_name", "Elon Musk"), &exec_2).await?;
     println!(
         "{} (few-shot CoT answer)",
         res.primary_textual_output().await.unwrap()
