ef8b1442bba942ca4016ddfaccea7fd459969f70
William Högman Rudenmalm <me@whn.se>
2023-05-27 22:47:23 +0200

    bugfix: remove unused is_empty

--- crates/llm-chain/src/document_stores/document_store.rs
+++ crates/llm-chain/src/document_stores/document_store.rs
@@ -17,14 +17,6 @@ where
 
     async fn next_id(&self) -> Result<T, Self::Error>;
 
-    /// Checks if the document store is empty
-    async fn is_empty(&self) -> Result<bool, Self::Error> {
-        match self.len().await {
-            Ok(length) => Ok(length == 0),
-            Err(e) => Err(e),
-        }
-    }
-
     async fn insert(&mut self, documents: &HashMap<T, Document<M>>) -> Result<(), Self::Error>;
 }
 
