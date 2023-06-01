7dace204f3ec9fcb5f25058548deb63dcfadf2b1
Emil Juzovitski <ejuzov@gmail.com>
2023-06-01 23:45:21 +0200

    exposed output stream

--- crates/llm-chain-llama/examples/stream.rs
+++ crates/llm-chain-llama/examples/stream.rs
@@ -1,3 +1,4 @@
+use llm_chain::output::StreamExt;
 use llm_chain::{executor, parameters, prompt};
 
 /// This example demonstrates how to use the llm-chain-llama crate to generate text using a
@@ -14,6 +15,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let res = prompt!("The Colors of the Rainbow are (in order): ")
         .run(&parameters!(), &exec)
         .await?;
-    println!("{}", res.to_immediate().await?);
+    let mut stream = res.as_stream().await?;
+    while let Some(v) = stream.next().await {
+        println!("{}", v);
+    }
+
     Ok(())
 }
