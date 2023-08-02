476b9e430f784cd4c4765a602ad6fe33858e5fbc
Shing Lyu <shinglyu@amazon.nl>
2023-08-02 14:35:32 +0000

    cleanup for PR

--- crates/llm-chain-mock/src/executor.rs
+++ crates/llm-chain-mock/src/executor.rs
@@ -1,33 +1,16 @@
 use async_trait::async_trait;
-use llm_chain::options;
-use llm_chain::options::{options_from_env, Opt, OptDiscriminants, Options, OptionsCascade};
+use llm_chain::options::Options;
 use llm_chain::output::Output;
 use llm_chain::prompt::Prompt;
 use llm_chain::tokens::{
     PromptTokensError, TokenCollection, TokenCount, Tokenizer, TokenizerError,
 };
 use llm_chain::traits::{ExecutorCreationError, ExecutorError};
-use thiserror::Error;
 
 /// Executor is responsible for running the LLM and managing its context.
 pub struct Executor {
+    #[allow(dead_code)]
     options: Options,
-}
-
-// TODO: cleanup
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("an invalid token was encountered during tokenization")]
-    /// During tokenization, one of the produced tokens was invalid / zero.
-    TokenizationFailed,
-    #[error("the context window is full")]
-    /// The context window for the model is full.
-    ContextFull,
-    #[error("reached end of text")]
-    /// The model has produced an end of text token, signalling that it thinks that the text should end here.
-    ///
-    /// Note that this error *can* be ignored and inference can continue, but the results are not guaranteed to be sensical.
-    EndOfText,
 }
 
 #[async_trait]
@@ -35,12 +18,11 @@ impl llm_chain::traits::Executor for Executor {
     type StepTokenizer<'a> = MockTokenizer;
     
     fn new_with_options(options: Options) -> Result<Self, ExecutorCreationError> {
-        Ok(Executor { options })
+        Ok(Executor { options: options })
     }
 
     async fn execute(&self, options: &Options, prompt: &Prompt) -> Result<Output, ExecutorError> {
-        // TODO: print options1
-        let mut output = format!( "As a mock large language model, I'm here to help you debug. I have received your prompt: \"{prompt}\".");
+        let output = format!( "As a mock large language model, I'm here to help you debug. I have received your prompt: \"{prompt}\" with options \"{options:?}\"");
         Ok(Output::new_immediate(Prompt::text(output)))
     }
 
@@ -49,11 +31,26 @@ impl llm_chain::traits::Executor for Executor {
         options: &Options,
         prompt: &Prompt,
     ) -> Result<TokenCount, PromptTokensError> {
-        unimplemented!();
+                let tokenizer = self.get_tokenizer(options)?;
+        let input = prompt.to_text();
+        let mut tokens_used = tokenizer
+            .tokenize_str(&input)
+            .map_err(|_e| PromptTokensError::UnableToCompute)?
+            .len() as i32;
+        let answer_prefix = self.answer_prefix(prompt);
+        if let Some(prefix) = answer_prefix {
+            let answer_used = tokenizer
+                .tokenize_str(&prefix)
+                .map_err(|_e| PromptTokensError::UnableToCompute)?
+                .len() as i32;
+            tokens_used += answer_used
+        }
+        let max_tokens = self.max_tokens_allowed(options);
+        Ok(TokenCount::new(max_tokens, tokens_used))
     }
 
     fn max_tokens_allowed(&self, _: &Options) -> i32 {
-        unimplemented!();
+        i32::MAX
     }
 
     fn answer_prefix(&self, _prompt: &Prompt) -> Option<String> {
@@ -61,24 +58,45 @@ impl llm_chain::traits::Executor for Executor {
     }
 
     fn get_tokenizer(&self, _: &Options) -> Result<Self::StepTokenizer<'_>, TokenizerError> {
-        unimplemented!();
+        Ok(MockTokenizer::new(self))
     }
 }
 
 pub struct MockTokenizer {}
 
 impl MockTokenizer {
-    pub fn new(executor: Executor) -> Self {
+    pub fn new(_executor: &Executor) -> Self {
         MockTokenizer {}
     }
 }
 
 impl Tokenizer for MockTokenizer {
     fn tokenize_str(&self, doc: &str) -> Result<TokenCollection, TokenizerError> {
-        unimplemented!()
+        let tokens: Vec<i32> = doc.as_bytes().to_vec().into_iter().map(|c| c as i32).collect();
+        Ok(tokens.into())
     }
 
     fn to_string(&self, tokens: TokenCollection) -> Result<String, TokenizerError> {
-        unimplemented!()
+        let bytes : Vec<u8> = tokens.as_i32().unwrap().into_iter().map(|c| c as u8).collect();
+        let doc = String::from_utf8(bytes).unwrap();
+        Ok(doc)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use llm_chain::traits::Executor;
+    #[test]
+    fn test_mock_tokenizer() {
+        let executor: crate::Executor = Executor::new_with_options(Options::empty().clone()).unwrap();
+        let tokenizer = executor.get_tokenizer(&executor.options).unwrap();
+        let tokens = tokenizer
+            .tokenize_str("Héllo world") //Notice that the UTF8 character translates to x3 i32s
+            .expect("failed to tokenize");
+        println!("{:?}", tokens);
+        assert_eq!(tokens.len(), 13);
+        let doc = tokenizer.to_string(tokens).expect("failed to convert back to string");
+        assert_eq!(doc, "Héllo world");
     }
 }
